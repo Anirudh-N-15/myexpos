7aef1de541fb822ea04754d289897144b8920bfd
Anirudh-N-15 <anirudhnambiar15@gmail.com>
2025-01-22 12:51:15 +0000

    Stage 14.

--- discript.sh
+++ discript.sh
@@ -2,18 +2,24 @@ cd /home/expos/myexpos/expl
 ./expl ./expl_progs/idle.expl
 ./expl ./expl_progs/init.expl
 ./expl ./expl_progs/exe.expl
+./expl ./expl_progs/primes.expl
 
 cd /home/expos/myexpos/spl
 echo "------------Startup Code"
 ./spl ./spl_progs/startup.spl
 echo "INT 7"
-./spl spl_progs/int7.spl
+./spl ./spl_progs/int7.spl
 echo "Timer"
-./spl spl_progs/timer.spl
+./spl ./spl_progs/timer.spl
 echo "------------Compiling Exception Handler"
-./spl spl_progs/exhandler.spl
+./spl ./spl_progs/exhandler.spl
 echo "Boot Module"
-./spl spl_progs/mod7boot.spl
+./spl ./spl_progs/mod7boot.spl
+echo "INT 10"
+./spl ./spl_progs/int10.spl
+echo "Scheduler Module"
+./spl ./spl_progs/mod5.spl
+
 
 cd /home/expos/myexpos/xfs-interface
 ./xfs-interface <<EOF
@@ -26,5 +32,8 @@ load --module 7 ../spl/spl_progs/mod7boot.xsm
 load --int=10 ../spl/spl_progs/exhandler.xsm
 load --library ../expl/library.lib
 load --exec ../expl/expl_progs/exe.xsm
+load --module 5 ../spl/spl_progs/mod5.xsm
+load --int=10 ../spl/spl_progs/int10.xsm
+load --exec ../expl/expl_progs/primes.xsm
 exit
 EOF
